700ed60185a3a694d2030fa88e8f59b0b67d4317
shrokmel <arvind.mc2@gmail.com>
2017-01-23 16:04:48 +0100

    changes to find_com_clusters_weight

--- examine_clusters.py
+++ examine_clusters.py
@@ -358,24 +358,37 @@ def find_com_clusters_weight(xcl, ycl, clusters, lx, ly, d):
     
     ### get the number of clusters
     
-    nclusters = len(clusters)   
+    nclusters = len(clusters)
+    
     xcm = np.zeros((nclusters), dtype=np.float64)
     ycm = np.zeros((nclusters), dtype=np.float64)
     
-    ### run over the clusters
+    ### Build weights array based on defect strengths
+    dweight = np.zeros(d.shape)
+    dtotal  = np.zeros(d.shape)         # normalization of weights
+                                        # sum of wghts of defects in a cluster=1
+    dweight[d<0] = 1./np.abs(d[d<0]+0.5)    # -1/2 defect weights
+    dweight[d>0] = 1./np.abs(d[d>0]-0.5)    # +1/2 defect weights
     
+    ### run over all clusters
     for i in range(nclusters):
-        
+        ### run over all points in the cluster
+        npts = len(clusters[i])
+        for j in range(npts):
+            mi = clusters[i][j]         # accesses particle index
+            dtotal[i] += dweight[mi]    # adds up total weight of all defects
+    
+    ### run over the clusters
+    for i in range(nclusters):
         ### run over the points in the cluster
-        
         npts = len(clusters[i])
         for j in range(npts):
             mi = clusters[i][j]     # this accesses the particle index
             # so d[mi] will give you the particle index
-            xcm[i] += xcl[mi]
-            ycm[i] += ycl[mi]
-        xcm[i] /= npts
-        ycm[i] /= npts
+            xcm[i] += xcl[mi]*dweight[mi]
+            ycm[i] += ycl[mi]*dweight[mi]
+        xcm[i] /= npts*dtotal[i]
+        ycm[i] /= npts*dtotal[i]
 
     ### put the center of masses back inside the box
     
